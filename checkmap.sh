d98bab3bb716011061056348c9490ae0c556a4dd
Mark Taylor <mataylo@sandia.gov>
2015-08-18 20:26:58 -0600

    add wrapper script for checkmap.ncl

---
+++ checkmap.sh
@@ -0,0 +1,18 @@
+#!/bin/tcsh -f
+#
+#  wrapper for regridfile.ncl
+#
+if ($#argv < 1 ) then
+   echo "checkmap.sh mapfile"
+   exit
+endif
+
+set mapfile = $1
+
+echo "mapfile: "  $mapfile
+
+set arg0 = wgtfile=\"$mapfile\"
+
+ncl  "$arg0" ~/codes/remap-ncl/checkmap.ncl
+
+
