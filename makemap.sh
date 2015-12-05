989dfe11020211f7466b659df5dbb518ffaa9ddc
Mark Taylor <mt@mp3dev.org>
2015-12-04 17:10:35 -0700

    tweaks for skybridge

--- makemap.sh
+++ makemap.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh -f
+#!/bin/tcsh 
 #
 #  wrapper for regridfile.ncl
 #
@@ -16,22 +16,29 @@ limit stacksize 512M
 set exe = /projects/ccsm/esmf-6.3.0rp1/bin/binO/Linux.intel.64.openmpi.default/ESMF_RegridWeightGen
 set esmf = "mpirun -np 8  $exe"
 
+# currently, NCL on redsky cant find ESMF path so NCL option doesn't work
+set use_ncl = 0
 
-# NCL on skybridge cant find ESMF path
-#set argsrc = src=\"$src\"
-#set argdst = dst=\"$dst\"
-#set argmap = base=\"$base\"
-#ncl  "$argsrc" "$argdst" "$argmap"  ~/codes/remap-ncl/make_esmfmap.ncl
+if ( $use_ncl ) then
+  set argsrc = src=\"$src\"
+  set argdst = dst=\"$dst\"
+  set argmap = base=\"$base\"
+  ncl  "$argsrc" "$argdst" "$argmap"  ~/codes/remap-ncl/make_esmfmap.ncl
+else
+  # run ESMF directly
+  # for MPAS source grids, add:  --src_regional -i
+  # on skybridge, needs this older library (or recompile ESMF):
+  module switch openmpi-intel/1.6
 
 
-# run ESMF directly
-# for MPAS source grids, add:  --src_regional -i
+  set map = {$base}_bilin.nc
+  $esmf   -d $dst -s $src  -w $map --method bilinear
 
-set map = {$base}_bilin.nc
-$esmf   -d $dst -s $src  -w $map --method bilinear
+   set map = {$base}_aave.nc
+   $esmf   -d $dst -s $src  -w $map --method conserve
 
-set map = {$base}_aave.nc
-$esmf   -d $dst -s $src  -w $map --method conserve
+   #set map = {$base}_patch.nc
+   #$esmf  -d $dst -s $src  -w $map --method patch
+endif
 
-#set map = {$base}_patch.nc
-#$esmf  -d $dst -s $src  -w $map --method patch
+
