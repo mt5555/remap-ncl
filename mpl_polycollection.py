930cacf6e24b9977054bc2edc2478be9b9fc7cb7
Mark Taylor <mt@mp3dev.org>
2024-06-19 05:12:27 -0600

    add simple polygon plotting script

---
+++ mpl_polycollection.py
@@ -0,0 +1,81 @@
+
+#
+# simpile routine to plot a function using matplotlib's polycollection
+#
+ 
+
+import time
+import numpy as np
+from netCDF4 import Dataset
+import cartopy.crs as ccrs
+import matplotlib
+from matplotlib import pyplot
+from matplotlib.collections import PolyCollection
+
+
+def shift_anti_meridian_polygons(lon_poly_coords, lat_poly_coords, eps=40):
+    """Shift polygons that are split on the anti-meridian for visualization
+    
+    Parameters
+    ----------
+    lon_poly_coords : ndarray(n, v)
+        longitudinal coordinates of each vertex of each polygon
+    lat_poly_coords : ndarray(n, v)
+        latitudinal coordinates of each vertex of each polygon
+    eps : float (default 10)
+        Tolerance for polygons to shift
+    
+    Returns
+    -------
+    polygons : ndarray(n, v, 2)
+        Combined longitude and latitude coordinates for all polygons (including shifted ones)
+    """
+    polygons = np.stack((lon_poly_coords, lat_poly_coords), axis=2)
+    diff = np.array(np.max(polygons[:,:,0], axis=1) - np.min(polygons[:,:,0], axis=1) > eps)
+    lon_coord_mask = polygons[:,:,0] < eps
+    lon_coord_mask[~diff,:] = 0
+
+    polygons[lon_coord_mask,0] = polygons[lon_coord_mask,0] + 360
+    return polygons
+
+
+def polyplot(xlat,xlon,area,outname):
+
+    mn=float(min(area))
+    mx=float(max(area))
+    clev=(mn,mx)
+    colormap='Spectral'
+    print(f"poly_plot(): plotting {len(area)} cells. data min/max= {mn:.3},{mx:.3}")
+    
+    # center plot at lon=0,lat=0:
+    proj=ccrs.PlateCarree()
+    xpoly  = proj.transform_points(proj, xlon, xlat)
+    
+    print("matplotlib/polycollection... ",end='')
+    dpi=600
+    start= time.time()
+    
+    # adjust cells into polycollection format:
+    xpoly = shift_anti_meridian_polygons(xpoly[:,:,0],xpoly[:,:,1])
+    corners=np.stack([xpoly[:,:,0],xpoly[:,:,1]],axis=2)
+    
+    ax = pyplot.axes(projection=ccrs.PlateCarree())
+    ax.set_global()
+    
+    fig=matplotlib.pyplot.figure()
+    ax = matplotlib.pyplot.axes(projection=proj)
+    ax.set_global()
+    p = matplotlib.collections.PolyCollection(corners, array=area, edgecolor='face',alpha=1)
+    p.set_clim(clev)
+    p.set_cmap(colormap)
+    ax.add_collection(p)
+    fig.colorbar(p)
+    
+    matplotlib.pyplot.savefig(outname,dpi=dpi,orientation="portrait",bbox_inches='tight')
+    end= time.time()
+    print(f"{end-start:.2f}s")
+    return 0
+
+
+
+
