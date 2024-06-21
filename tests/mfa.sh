49157c9f6304e33b822b60f108cf77bb2e11cf67
Mark Taylor <mt@mp3dev.org>
2024-06-21 14:02:07 -0600

    added ne256 trigrid testcase

--- tests/mfa.sh
+++ tests/mfa.sh
@@ -4,17 +4,21 @@
 #  
 #
 
-wdir="$HOME/codes/PreAndPostProcessingScripts/regridding/map_file_analysis/test/maps"
-#python mfa.py a2o \
-#       $wdir/map_ne4pg2_to_oQU480_bilin.200527.nc \
+# ultra lowres  test from Pre&Post:
+#wdir="$HOME/codes/PreAndPostProcessingScripts/regridding/map_file_analysis/test/maps"
+#python mfa.py o2a \
+#       $wdir/map_oQU480_to_ne4pg2_mono.200527.nc  \
 #       $wdir/map_oQU480_to_ne4pg2_mono.200527.nc  \
 #       $wdir/domain.lnd.ne4pg2_oQU480.200527.nc
 
 
-python mfa.py o2a \
-       $wdir/map_oQU480_to_ne4pg2_mono.200527.nc  \
-       $wdir/map_oQU480_to_ne4pg2_mono.200527.nc  \
-       $wdir/domain.lnd.ne4pg2_oQU480.200527.nc
+#ne256 trigrid with speckling:m
+wdir="/sems-data-store/ACME/inputdata"
+python mfa.py l2a \
+       $wdir/cpl/gridmaps/ne256pg2/map_r0125_to_ne256pg2_bilin.200212.nc \
+       $wdir/cpl/gridmaps/ne256pg2/map_oRRS18to6v3_to_ne256pg2_nco.200212.nc \
+       $wdir/share/domains/domain.lnd.r0125_oRRS18to6v3.200212.nc
+
 
 
 
