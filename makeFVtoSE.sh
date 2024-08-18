e3d88ca35731ddc23be246e28cfbc0286065446f
Mark Taylor <mt@mp3dev.org>
2024-08-18 14:15:25 -0600

    add mono option to makeFVtoSE

--- makeFVtoSE.sh
+++ makeFVtoSE.sh
@@ -30,9 +30,34 @@ grid1=$3
 name2=$4
 grid2=$5
 
+if [ ! -x  $exepath/GenerateOfflineMap ]; then
+    echo TR utilties missing
+    exit 1
+fi
+if [ ! -f $grid1 ]; then
+    if [ -f $wdir/grids/$grid1 ] ; then
+        grid1=$wdir/grids/$grid1
+    else
+        echo ERROR missing: $grid1
+        exit 1
+    fi
+fi
+if [ ! -f $grid2 ]; then
+    if [ -f $wdir/grids/$grid2 ] ; then
+        grid2=$wdir/grids/$grid2
+    else
+        echo ERROR missing: $grid2
+        exit 1
+    fi
+fi
+
+
+
+
+
 
 # check TR utilties, existence of grids, make overlap if needed
-make_overlap.sh $name1 $grid1 $name2 $grid2  || exit 1
+./make_overlap.sh $name1 $grid1 $name2 $grid2  || exit 1
 overlap=$wdir/maps/overlap_${name1}_${name2}.g
 if [ ! -f $overlap ]; then
     overlap=$wdir/maps/overlap_${name2}_${name1}.g
@@ -40,7 +65,7 @@ fi
 
 
 case "$maptype" in
-    monotr)
+    mono)
         algarg="--mono --correct_areas" ;;
     *)
         echo "bad maptype  $maptype" ;  exit 1 ;;
@@ -53,9 +78,16 @@ if [ -f $map ]; then
     echo found $map
     echo resusing this file and skippng GenerateOfflineMap
 else
-    # first SE->FV mono:
-    ./makeSEtoFV mono $name2 $grid2 $name1 $grid1
-    # take tranpose to get monotr map for grid1->grid2
-    ./makeTR mono $name2 $grid2 $name1 $grid1
+    echo "GenerateOfflineMap: $maptype"
+    echo "log file: $map_log"
+    rm -f $map_log
+    $exepath/GenerateOfflineMap --in_mesh $grid1  --out_mesh $grid2  --ov_mesh $overlap \
+                                --out_type cgll --out_np 4  --in_type fv  \
+                                --out_double --out_format Netcdf4 \
+                                $algarg  --out_map $map >& $map_log
+    if [ ! -f $map ]; then
+        echo GenerateOfflineMap failed
+        exit 1
+    fi
 fi
 
